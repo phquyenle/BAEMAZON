fb28cc8b61cff04e46f0bb612ba18e88cbce714a
Quyen Le <heyquyenle@gmail.com>
2020-04-14 23:20:53 -0700

    setting up

---
+++ bamazonschema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+	item_id INT(4) NOT NULL,
+	product_name VARCHAR(100) NOT NULL,
+	department_name VARCHAR(100) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INT(20) NOT NULL,
+	PRIMARY KEY (item_id)
+);
