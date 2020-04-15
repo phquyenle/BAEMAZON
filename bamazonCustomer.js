85291b29ad77aa927f1a48a445164ff2af7f370b
Quyen Le <heyquyenle@gmail.com>
2020-04-15 00:57:34 -0700

    finalize the bamazoncustomer.js
    The table is working
    the inquirer is working
    table inventory is updating on it own
    thinking of adding a csv instead of using INSERT INTO

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,5 +13,97 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
 if (err) throw err;
 console.log ("connected as id" + connection.threadId);
+availableProducts();
 });
 
+
+var availableProducts = function(){
+    var queryStr= "SELECT * FROM products";
+    connection.query(queryStr,function(err,res){
+        if (err) throw err;
+        var productsTable= new Table ({
+            head:["Item ID:", "Product Name:", "Category:" , "Price","Quantity:"],
+            colWidths:[10,25,25,10,14]
+        });
+        for (var i=0; i < res.length; i++){
+            productsTable.push(
+            [res[i].item_id,
+            res[i].product_name,
+            res[i].department_name,
+            res[i].price, 
+            res[i].stock_quantity]
+            );
+        }
+        console.log(productsTable.toString());
+        buyingPrompt();
+    });
+}
+
+var buyingPrompt= function(){
+    inquirer.prompt([
+        {
+            name:"productID",
+            type:"input",
+            message:"Enter ID for item you like to purchase",
+          validate: function(value){
+              if (isNaN (value) === false){
+                  return true;
+              }
+              return false;
+          }
+        },
+        {
+            name: "productUnits",
+            type:"input",
+            message:"How many item units do you like to purchase?",
+            validate: function (value){
+                if (isNaN (value) === false){
+                return true;
+            }
+            return false;
+            }
+
+    }]).then(function(inputs){
+        var query = "Select stock_quantity, price, product_sales, department_name FROM products WHERE ?";
+		connection.query(query, { item_id: inputs.productID}, function(err, res) {
+			if (err) throw err;
+
+			var available_stock = res[0].stock_quantity;
+			var price_per_unit = res[0].price;
+			var productSales = res[0].product_sales;
+			var productDepartment = res[0].department_name;
+            if (available_stock >= inputs.productUnits) {
+            purchase(available_stock,price_per_unit, productSales, productDepartment,
+                inputs.productID,inputs.productUnits);
+            } else {
+                console.log("Items are not available.");
+                buyingPrompt();
+    }
+});
+});
+};
+var purchase= function(availableStock, price, productSales, productDepartment, selectedProductID, selectedProductUnits) {
+	
+	// Updates stock quantity once purchase complete.
+	var updatedStockQuantity = availableStock - selectedProductUnits;
+
+	// Calculates total price for purchase based on unit price, and number of units.
+	var totalPrice = price * selectedProductUnits;
+
+	// Updates total product sales.
+	var updatedProductSales = parseInt(productSales) + parseInt(totalPrice);
+	
+	// Updates stock quantity on the database based on user's purchase.
+	var query = "UPDATE products SET ? WHERE ?";
+	connection.query(query, [{
+		stock_quantity: updatedStockQuantity,
+		product_sales: updatedProductSales
+	}, {
+		item_id: selectedProductID
+	}], function(err, res) {
+
+		if (err) throw err;
+		console.log("Yay, your purchase is complete.");
+		console.log("payment has been received in the amount of : " + totalPrice);
+availableProducts();
+		
--- bamazonschema.sql
+++ bamazonschema.sql
@@ -12,3 +12,25 @@ CREATE TABLE products(
 	stock_quantity INT(20) NOT NULL,
 	PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
+VALUES (123, "sandals", "shoes", 89.99, 25),
+	   (456, "jerseys", "clothing", 59.99, 10),
+	   (789, "heels", "shoes", 59.99, 50),
+	   (420, "sweaters", "clothing", 29.99, 45),
+	   (120, "pants", "clothing", 39.99, 15),
+	   (609, "shorts", "clothing", 19.99, 19),
+	   (720, "earrings", "jewelry", 49.99, 11),
+	   (808, "necklaces", "jewelry", 69.99, 10),
+	   (913, "bats", "sport", 9.99, 19),
+	   (109, "basketball", "sport", 19.99, 27)
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NOT NULL,
+  over_head_costs DECIMAL(7,2) NOT NULL DEFAULT '0.00',
+  total_sales DECIMAL(7,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (department_id)
+);
+Select * From departments;
+
+ALTER TABLE products ADD COLUMN product_sales DECIMAL(7,2) DEFAULT '0.00';
